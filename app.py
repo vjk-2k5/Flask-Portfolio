8ecdcde09bdebe770d50b664d2767ce0723dbf0e
vjk-2k5 <svijaykrishna12@gmail.com>
2024-10-09 23:34:08 +0530

    adding function

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    if request.method == 'POST':
+        num1 = request.form.get('num1', type=float)
+        num2 = request.form.get('num2', type=float)
+        operation = request.form.get('operation')
+        
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            result = num1 / num2 if num2 != 0 else "Error: Division by zero"
+        else:
+            result = "Invalid operation"
+        
+        return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
